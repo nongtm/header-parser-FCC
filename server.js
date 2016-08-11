fe3305f506f427917841baa5cd97e9409abcd5f6
Tri Nong <minhtri1296@gmail.com>
2016-08-11 19:22:19 +0000

    New Version

--- server.js
+++ server.js
@@ -7,16 +7,9 @@ const osname = require('os-name')
 var getos = require('getos')
 var platform = require('platform');
 
-var locale = require('browser-locale')
-var language = require('browser-language');
-
 app.get('/', function (req, res) {
     var myIP = ip.address();
-    
-    getos(function(e,os) {
-  if(e) return console.log(e)
-  console.log("Your OS is:" +JSON.stringify(os)  )
-})
+
   var obj = {
     "ipaddress": myIP,
     "language": req.headers['accept-language'].split(',')[0],
